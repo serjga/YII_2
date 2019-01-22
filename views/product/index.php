852d31ee71c7bc1edea82e56eabf41df73f4f10b
serjga <serg862000@gmail.com>
2019-01-22 21:48:51 +0200

    homework_lesson_2

---
+++ views/product/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php Pjax::begin(); ?>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+             [
+                'attribute' => 'name',
+                'value' => function($model) {
+                    return Html::a($model->name, ['product/view', 'id' => $model->id]);
+                },
+                'format' => 'html'
+            ],           
+            'price',
+
+            [
+                'attribute' => 'created_at',
+                'format'=> 'datetime',
+                'contentOptions' => ['class' => 'small']
+            ],
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
+</div>
